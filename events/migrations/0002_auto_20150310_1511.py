f54856d6aa8400566455e2af565022761a0385a1
nils jacobson <njacobson@internet2.edu>
2015-03-10 15:28:34 -0400

    adding new migrations: :

---
+++ events/migrations/0002_auto_20150310_1511.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('events', '0001_initial'),
+        ('sites', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('taggit', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='eventspluginmodel',
+            name='tags',
+            field=models.ManyToManyField(to='taggit.Tag', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='event',
+            name='author',
+            field=models.ForeignKey(verbose_name='author', blank=True, to=settings.AUTH_USER_MODEL, null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='event',
+            name='calendar',
+            field=models.ForeignKey(related_name='events', blank=True, to='events.Calendar', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='event',
+            name='sites',
+            field=models.ManyToManyField(to='sites.Site'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='event',
+            name='tags',
+            field=models.ManyToManyField(to='taggit.Tag', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterUniqueTogether(
+            name='event',
+            unique_together=set([('event_date', 'slug')]),
+        ),
+    ]
---
+++ events/migrations/0003_auto_20150310_1512.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('events', '0002_auto_20150310_1511'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='event',
+            name='publish_date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 3, 10, 15, 12, 16, 92936), verbose_name='publication date'),
+            preserve_default=True,
+        ),
+    ]
