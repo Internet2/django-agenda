a7d1265bd7929bc878af8fdc004ff75f14ec4f03
nils jacobson <njacobson@internet2.edu>
2015-08-26 14:25:28 -0400

    adding migration for 1.8

---
+++ events/migrations/0004_auto_20150820_1317.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('events', '0003_auto_20150310_1512'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='event',
+            name='publish_date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 8, 20, 13, 17, 18, 804816), verbose_name='publication date'),
+            preserve_default=True,
+        ),
+    ]
