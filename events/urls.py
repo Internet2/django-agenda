2a33c4849e7a54150093265d71cb01be63b7137f
Nils Jacobson <njacobson@eduroam-wlan-159.internet2.edu>
2017-07-11 09:20:26 -0400

    updating urls.py for 1.10

--- events/urls.py
+++ events/urls.py
@@ -2,19 +2,21 @@ from django.conf.urls import *
 
 from models import *
 
+from . import views
+
 info_dict = {
     'queryset'                  : Event.published.all(),
     'date_field'                : 'event_date',
     'template_object_name'      : 'event',
 }
 
-urlpatterns = patterns('events.views.date_based',
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$', 'object_detail', info_dict,  name='agenda-detail'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$',                  'archive',       info_dict,  name='agenda-archive-day'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$',                                   'archive',       info_dict,  name='agenda-archive-month'),
-    url(r'^(?P<year>\d{4})/$',                                                      'archive',       info_dict,  name='agenda-archive-year'),
-    url(r'^$',                                                                      'index',         info_dict,  name='agenda-index'),
-)
+urlpatterns = [
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$', views.date_based.object_detail, info_dict,  name='agenda-detail'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$',                  views.date_based.archive,       info_dict,  name='agenda-archive-day'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$',                                   views.date_based.archive,       info_dict,  name='agenda-archive-month'),
+    url(r'^(?P<year>\d{4})/$',                                                      views.date_based.archive,       info_dict,  name='agenda-archive-year'),
+    url(r'^$',                                                                      views.date_based.index,         info_dict,  name='agenda-index'),
+]
 
 ical_dict = {
     'queryset'                  : info_dict['queryset'],
@@ -26,11 +28,11 @@ ical_dict = {
     'end_time_field'            : 'end_time',
 }
 
-urlpatterns += patterns('events.views.vobject_django',
-    url(r'^calendar.ics$',                                                          'icalendar',     ical_dict,  name='agenda-icalendar'),
-)
+urlpatterns += [
+    url(r'^calendar.ics$',                                                          views.vobject_django.icalendar,     ical_dict,  name='agenda-icalendar'),
+]
 
-urlpatterns = patterns('events.views.custom',
-    url(r'^by_tags/(?P<calendar_list>[^//]+)/(?P<tag_list>[^//]+)/(?P<ordering>[^//]+)/(?P<start_date_range>[^//]+)/(?P<end_date_range>[^//]+)/(?P<page>[^//]+)$', 'by_tags'),
-)
+urlpatterns = [
+    url(r'^by_tags/(?P<calendar_list>[^//]+)/(?P<tag_list>[^//]+)/(?P<ordering>[^//]+)/(?P<start_date_range>[^//]+)/(?P<end_date_range>[^//]+)/(?P<page>[^//]+)$', views.custom.by_tags),
+]
 
